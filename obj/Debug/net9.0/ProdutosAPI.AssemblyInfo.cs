a7cca03ad6480a610d19ba4826d3d97c9be1be34
Miltonjss <149200599+Miltonjss@users.noreply.github.com>
2026-05-20 16:20:04 -0400

    Atualizando arquivos e configurações

--- obj/Debug/net9.0/ProdutosAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProdutosAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98c8fb95026e4393a2327742939cd64cd966203")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProdutosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
